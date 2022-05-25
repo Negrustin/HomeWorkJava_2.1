3150e81a66e2de893a311bd162bcab4f7dd4e3c1
Negrustin <negrustin87@gmail.com>
2022-05-25 18:49:33 +0200

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
 
-            public static void main(String[] args) {
+    public static void main(String[] args) {
 
-                int ticletCost = 1000;
-                int conditionBonus = 20;
-                int bonusMiles = ticletCost/conditionBonus;
-                System.out.println("Колличество бонусных миль: " + bonusMiles);
-            }
-        }
+        int ticletCost = 1000;
+        int conditionBonus = 20;
+        int bonusMiles = ticletCost / conditionBonus;
+        System.out.println("Колличество бонусных миль: " + bonusMiles);
+    }
+}
 
 
