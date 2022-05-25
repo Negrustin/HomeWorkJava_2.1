5567970324bd14b55e75ae3a268155d28333b666
Negrustin <negrustin87@gmail.com>
2022-05-25 18:52:53 +0200

    Renaming variables

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int ticletCost = 1000;
+        int ticketCost = 1000;
         int conditionBonus = 20;
-        int bonusMiles = ticletCost / conditionBonus;
+        int bonusMiles = ticketCost / conditionBonus;
         System.out.println("Колличество бонусных миль: " + bonusMiles);
     }
 }
